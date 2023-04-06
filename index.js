433c20f28f73831eea3a7952f3c41406b2af526a
ranasingh99 <sanketverma852@gmail.com>
2023-04-06 12:05:53 +0530

    creating and inserting in dom

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-var itemList = document.querySelector('#items');
+//creating new Element
+//1) creating new div
+var newDiv = document.createElement('div');
 
-itemList.children[1].style.backgroundColor = 'red';
-//firstElementChild
-console.log(itemList.firstElementChild);
-itemList.firstElementChild.textContent = 'firstLi1'
-// lastchild
-itemList.lastChild.textContent='lastchild';
-//lastElementChild
-itemList.lastElementChild.style.backgroundColor = 'yellow';
-//nextsibling
-console.log(itemList.nextSibling);
-//nextElementSibling
-console.log(itemList.nextElementSibling);
-//previousSibling
-console.log(itemList.previousSibling);
-//previousElementSibling
-console.log(itemList.previousElementSibling);
-itemList.previousElementSibling.style.color = 'blue';
+//Add class
+newDiv.className = 'hello';
+// Add id
+newDiv.id = 'hello1';
+
+// Add Attribute
+
+newDiv.setAttribute('title','hello title');
+
+//createTextNode
+
+var newDivText = document.createTextNode('Namaste');
+
+// add text to div
+
+newDiv.appendChild(newDivText);
+
+//insert into dom
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+container.insertBefore(newDiv,h1);
+console.log(newDiv);
