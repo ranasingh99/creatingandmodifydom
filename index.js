894a638b7965720845f6d03ea14c43ecf9873dfa
ranasingh99 <sanketverma852@gmail.com>
2023-04-06 12:44:37 +0530

    assignment

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-//creating new Element
-//1) creating new div
-var newDiv = document.createElement('div');
+// //creating new Element
+// //1) creating new div
+// var newDiv = document.createElement('div');
 
-//Add class
-newDiv.className = 'hello';
-// Add id
-newDiv.id = 'hello1';
+// //Add class
+// newDiv.className = 'hello';
+// // Add id
+// newDiv.id = 'hello1';
 
-// Add Attribute
+// // Add Attribute
 
-newDiv.setAttribute('title','hello title');
+// newDiv.setAttribute('title','hello title');
 
-//createTextNode
+// //createTextNode
 
-var newDivText = document.createTextNode('Namaste');
+// var newDivText = document.createTextNode('Namaste');
 
-// add text to div
+// // add text to div
 
-newDiv.appendChild(newDivText);
+// newDiv.appendChild(newDivText);
 
-//insert into dom
+// //insert into dom
 
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
 
-container.insertBefore(newDiv,h1);
-console.log(newDiv);
+// container.insertBefore(newDiv,h1);
+// console.log(newDiv);
+
+//Assignment
+
+var parentnode = document.getElementById('items');
+parentnode.innerHTML = '<li>Hello World</li>' + parentnode.innerHTML;
+
+var parentnode1 = document.getElementById('main-header');
+parentnode1.innerHTML = '<h3>Hello World</h3>' + parentnode1.innerHTML;
